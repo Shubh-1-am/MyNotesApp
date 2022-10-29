99f53824b474d457309684b62160f12ae398025e
Shubh-1-am <aspiretoinspire2004@gmail.com>
2022-10-30 01:13:11 +0530

    Added update functionality.

--- NotesApp/app/src/main/java/com/example/notesapp/CustomAdapter.java
+++ NotesApp/app/src/main/java/com/example/notesapp/CustomAdapter.java
@@ -2,12 +2,16 @@ package com.example.notesapp;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -45,6 +49,52 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomViewHolder> {
                 return true;
             }
         });
+
+        holder.llRow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateItem(i);
+            }
+        });
+    }
+
+    private void updateItem(int i) {
+        mHelper.mNoteDao().deleteNote(new Note(mList.get(i).getId(),
+                mList.get(i).getTitle(),mList.get(i).getContent()));
+        
+        ((MainActivity)mContext).showNotes();
+
+        Dialog dialog = new Dialog(mContext);
+        dialog.setContentView(R.layout.add_note_layout);
+
+        EditText Edt_title, Edt_Content;
+        Button Btn_add;
+
+        Edt_title = dialog.findViewById(R.id.edt_title);
+        Edt_title.setText(mList.get(i).getTitle());
+        Edt_Content = dialog.findViewById(R.id.edt_content);
+        Edt_Content.setText(mList.get(i).getContent());
+        Btn_add =   dialog.findViewById(R.id.btn_add);
+
+
+        Btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String title = Edt_title.getText().toString();
+                String content = Edt_Content.getText().toString();
+
+                if (!content.equals("")) {
+                    mHelper.mNoteDao().addNote(new Note(title,content));
+                    ((MainActivity)mContext).showNotes();
+                    dialog.dismiss();
+
+                } else {
+                    Toast.makeText(mContext, "Please Enter Something..", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        dialog.show();
     }
 
     private void deleteItem(int pos) {
